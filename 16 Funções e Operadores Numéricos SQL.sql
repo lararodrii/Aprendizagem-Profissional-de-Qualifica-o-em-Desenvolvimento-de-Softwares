56aba546da3a76e062cfcaaa210af6ecf3c4422d
lararodrii <rodrigueslarab@gmail.com>
2024-08-26 16:34:26 -0300

    Add files via upload

---
+++ 16 Funções e Operadores Numéricos SQL.sql
@@ -0,0 +1,19 @@
+/*Funções e Operadores Numéricos SQL*/
+select 1;
+
+select 1 + 1;
+
+select - (2);
+
+select 4 div 2;
+
+select abs(-2);
+
+select pow(2 , 3);
+
+select * from tb_cli 
+where cli_nome like '%Ana%'; 
+
+/*Funções Data e Hora MySQL*/
+select now();
+select year('2020-09-21 23:33:25');
